f058261522d77af99bf83d692f30ebc2e0dc46bc
tsundDev <benlmartin.work@gmail.com>
2021-12-27 15:50:52 +0000

    Base Application - Upgraded Dependancies - NSIS For Windows - Tested - Auto-update working / TODO: LINUX AND MAC SUPPORT + APP DEVELOPMENT

---
+++ public/electron.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const { autoUpdater } = require('electron-updater');
+
+autoUpdater.logger = require('electron-log');
+autoUpdater.logger.transports.file.level = 'info';
+
+function createWindow() {
+    // Create the browser window.
+    const win = new BrowserWindow({
+        title: 'WALDO Desktop',
+        
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+    });
+
+    win.setMenu(null);
+    // and load the index.html of the app.
+    // win.loadFile("index.html");
+    win.loadURL(
+        isDev
+            ? 'http://localhost:3000' 
+            : `file://${path.join(__dirname, '../build/index.html')}`
+    );
+    // Open the DevTools.
+    if (isDev) {
+        win.webContents.openDevTools({ mode: 'detach' });
+    }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+});
+
+autoUpdater.on('update-avaliable', (info) => {
+    console.log('Update avaliable.');
+    console.log("Version: " + info.version);
+    console.log("Release Date: " + info.releaseDate);
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    console.log("Running latest version.");
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+    console.log("Download speed: " + progressObj.bytesPerSecond);
+    console.log("Downloaded: " + progressObj.percent + "%");
+    console.log("Expected total size: " + progressObj.total);
+    console.log("Downloaded: " + progressObj.transferred);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    console.log("Update downloaded");
+    autoUpdater.quitAndInstall();
+});
+
+if (!isDev) {
+    autoUpdater.checkForUpdates();
+}
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      this is an app test
+    </div>
+  );
+}
+
+export default App;
